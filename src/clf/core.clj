605e166bbec9ba88fd6b0213fb4374c5b0af26a7
scouri <eneblank19@protonmail.com>
2022-07-02 18:01:28 +0000

    optimizations made + color output

--- src/clf/core.clj
+++ src/clf/core.clj
@@ -1,14 +1,34 @@
 (ns clf.core
   (:import [java.io File])
   (:require [clojure.string :as str]
+            [io.aviso.ansi :as ansi]
             [clojure.tools.cli :refer [parse-opts]]))
+
+(defn colorize-output
+  [path]
+  (let [split (str/split path #"\n")]
+    (doseq [item split]
+      (println 
+       (if (.isDirectory (File. item))
+        (ansi/bold-cyan (.getName (File. item)))
+        (.getName (File. item)))))))
+  
+      ;;  (map (fn [x]
+      ;;         ((if (.isDirectory x) ansi/bold-cyan ansi/white)
+      ;;          (.getName x))))))
 
 (defn list-files
   [directory]
   (->> (.listFiles (File. directory))
        (filter #(not (.isHidden %)))
-       (sort)
-       (map #(.getName %))
+       (sort-by (fn [f] (.getName f)))
+       (str/join \newline)))
+
+(defn get-path
+  [directory]
+  (->> (.listFiles (File. directory))
+       (filter #(not (.isHidden %)))
+       (sort-by (fn [f] (.getAbsolutePath f)))
        (str/join \newline)))
 
 (defn list-hidden
@@ -19,12 +39,10 @@
        (str/join \newline)))
 
 (defn list-recursive
-  [directory] 
+  [directory]
   (let [dir? #(.isDirectory %)]
     (->> (tree-seq dir? #(.listFiles %) (File. directory))
          (filter (comp not dir?))
-         (sort) 
-         (map #(.getPath %)) 
          (str/join \newline))))
 
 (defn- usage
@@ -39,27 +57,19 @@
 
 (def cli-options
   [["-a" "--all" "do not ignore entries starting with ."]
-   ["nil" "--test"] 
+   ["nil" "--test"]
    ["-R" "--recursive" "list subdirectories recursively"]
+   ["-C" "--colorize" "use terminal colors for output"]
    ["-h" "--help" "Print this help information"]])
 
 (defn -main [& args]
   (let [{:keys [options arguments summary]} (parse-opts args cli-options)]
-    (println arguments)
-    (println options)
-    (if (empty? (second arguments))
-      (def directory ".")
-      (def directory (second arguments)))
-
-    (cond
-      (:help options)
-      (usage summary)
-
-      (:all options)
-      (println (list-hidden directory))
-
-      (:recursive options)
-      (println (list-recursive directory)))
-
-      (empty? options)
-      (println (list-files directory))))
+    (let [directory (if (str/blank? (second arguments))
+                      "."
+                      (second arguments))]
+      (case options
+        :help (usage summary)
+        :all (println (list-hidden directory))
+        :recursive (println (list-recursive directory))
+        :colorize (colorize-output (get-path directory))
+        (println (list-files directory))))))
